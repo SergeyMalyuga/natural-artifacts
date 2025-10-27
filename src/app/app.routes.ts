0ca9ce3da4323e55af621e66cfcae45f9d55144f
Sergey Malyuga <sergey@malyuga.ru>
2025-10-27 20:21:39 +0300

    Подключить шрифты к проекту.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppRoute } from './core/constants/const';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: AppRoute.MAIN,
+    title: 'Main',
+    loadComponent: () => import('./pages/main/main.component').then((m) => m.MainComponent),
+  },
+];
