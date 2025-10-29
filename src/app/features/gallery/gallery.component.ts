f1260d99e76008cc8f4e1426a02814e94aa048f9
Sergey Malyuga <sergey@malyuga.ru>
2025-10-29 12:51:46 +0300

    Создать gallery.component

---
+++ src/app/features/gallery/gallery.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  imports: [],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GalleryComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
-import {HeroComponent} from '../../features/hero/hero.component';
+import { HeroComponent } from '../../features/hero/hero.component';
+import { GalleryComponent } from '../../features/gallery/gallery.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, GalleryComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
