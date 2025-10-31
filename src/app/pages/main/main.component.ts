972d617498e181c8c74de813aafa57a2263af4c4
Sergey Malyuga <sergey@malyuga.ru>
2025-10-31 20:40:33 +0300

    Создать about.component. Завершить вёрстку about.component

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { GalleryComponent } from '../../features/gallery/gallery.component';
+import { AboutComponent } from '../../features/about/about.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, GalleryComponent],
+  imports: [HeaderComponent, HeroComponent, GalleryComponent, AboutComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
